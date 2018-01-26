41135a6fc73aa48a9488f4502daf701bf188e47c
huluobo <hujewelz@163.com>
2018-01-26 11:08:01 +0800

    initial commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Ccmark",
+    pkgConfig: "libcmark",
+    providers: [
+        .brew(["cmark"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+)
